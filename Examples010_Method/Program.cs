ee936f83832c788d78eaffb445f390af23f6ad9b
Tatyana <kta995@mail.ru>
2022-11-25 22:46:35 +0300

    Ex-s 008.009.010

---
+++ Examples010_Method/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {182, 18, 3, 4, 57, 6, 57, 18, 19};
+
+int n = array.Length;
+int find = 18;
+int index = 0;
+
+while (index < n)
+{
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+  
+  
+    index++;
+}

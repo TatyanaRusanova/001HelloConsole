677e5bd7c82e33d57b2732f5e132cae9b9af26ae
Tatyana <kta995@mail.ru>
2022-11-26 11:14:51 +0300

    lesson2Test2

--- Lesson2_Test/Program.cs
+++ Lesson2_Test/Program.cs
@@ -2,8 +2,8 @@
 Console.WriteLine($"Сгенерировано случайное число {num}");
 
 
-int x1 = (int)Math.Pow(10, 3 - 3);
-int x3 = (int)Math.Pow(10, 3 - 1);
+//int x1 = (int)Math.Pow(10, 3 - 3); //находит разряд числа (это 1разряд)
+// int x3 = (int)Math.Pow(10, 3 - 1); //3 разряд
 
 int result = num/100;  //количество сотен
 int result1 = num % 10; //количество единиц
